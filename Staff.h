82dda6254709f4d8d51d02f5be7634c96d4e601c
Saranasuriya N.V <100509136+IT21310478@users.noreply.github.com>
2022-05-19 23:55:26 +0530

    update Staff.h

---
+++ Staff.h
@@ -0,0 +1,23 @@
+#include "Enrolment.h"
+#define SIZE 5
+
+class Staff{
+  protected:
+    int SatffId;
+    char Staffnic[12];
+    char Staffname[20];
+    char Staffaddress[100];
+    char Staffemail[30];
+    char Staffphone[10];
+
+    Enrolment* apt[SIZE];
+
+  public:
+    Staff();
+    Staff(int pStaffId, char pStaffNic[12], char pStaffName[20], char pStaffAddress[100], char pStaffEmail[30], int pStaffPhone[10]);
+    void updateStaffDetails();
+    void viewCurrentDetails();
+    void manage( Enrolment* papt);
+    ~Staff();
+
+};
